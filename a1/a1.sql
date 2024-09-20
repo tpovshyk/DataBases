035b1db649bf726495fbd8992a0cbd80883f99b1
Повшик Тетяна <150916411+tpovshyk@users.noreply.github.com>
2024-09-20 19:10:59 +0300

    Update a1.sql

--- a1/a1.sql
+++ a1/a1.sql
@@ -1,10 +1,22 @@
-WITH OrderQuantities AS (
-	SELECT ClientID, SUM(Quantity) AS TotalQuantity
+WITH ORDERS1 AS (
+	SELECT *
 	FROM ORDERS
 	WHERE Quantity > 1
-	GROUP BY ClientID
 )
-SELECT ClientID, TotalQuantity
-FROM OrderQuantities
-ORDER BY TotalQuantity DESC;
+SELECT 
+    CLIENTS.ClientName,
+    PRODUCTS.ProductName,
+    ORDERS1.Quantity,
+    PAYMENTS.Amount,
+    SHIPMENTS.DeliveryAddress
+FROM ORDERS1
+JOIN CLIENTS ON ORDERS1.ClientID = CLIENTS.ClientID
+JOIN PRODUCTS ON ORDERS1.ProductID = PRODUCTS.ProductID
+JOIN PAYMENTS ON ORDERS1.OrderID = PAYMENTS.OrderID
+JOIN SHIPMENTS ON ORDERS1.OrderID = SHIPMENTS.OrderID
+WHERE CLIENTS.ClientName = 'Laura Walker'  
+-- GROUP BY CLIENTS.ClientName, PRODUCTS.ProductName, ORDERS1.Quantity, PAYMENTS.Amount, SHIPMENTS.DeliveryAddress
+ORDER BY PAYMENTS.Amount DESC;
+
+
 
